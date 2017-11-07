12a8b7f0a0db1c9858eb4d10112950f21ba211a3
Matteo Bracci <matteobracci1@gmail.com>
2017-11-06 16:32:03 -0800

    format

--- src/OpenGP/GL/DepthSurfaceRenderer.cpp
+++ src/OpenGP/GL/DepthSurfaceRenderer.cpp
@@ -31,7 +31,6 @@ const char *DepthSurfaceRenderer::vshader() {
 
 			vec3 pos = vec3((sensor_matrix_inv * vec4(vposition, 1)).xy * depth, depth);			
 
-            //gl_Position = get_MV() * vec4(pos, 1.0);
 			//if (depth == zfar) gl_Position.z = -zfar;
             //gl_Position = get_P() * gl_Position;
             
@@ -71,89 +70,89 @@ const char *DepthSurfaceRenderer::fshader() {
 DepthSurfaceRenderer::DepthSurfaceRenderer() { rebuild(); }
 
 void DepthSurfaceRenderer::rebuild_mesh() {
-  std::vector<Vec3> vposition;
-  std::vector<unsigned int> triangles;
+	std::vector<Vec3> vposition;
+	std::vector<unsigned int> triangles;
 
-  GLuint n = 0;
-  for (GLuint i = 0; i < width; i++) {
-    for (GLuint j = 0; j < height; j++) {
-      float x = 2 * (float)i / (float)(width - 1) - 1;
-      float y = 2 * (float)j / (float)(height - 1) - 1;
-      Vec3 vert(x, y, 0);
-      vposition.push_back(vert);
-      if (i > 0 && j > 0) {
-        GLuint v0 = n;
-        GLuint v1 = v0 - 1;
-        GLuint v2 = v0 - height;
-        GLuint v3 = v1 - height;
-        triangles.push_back(v0);
-        triangles.push_back(v1);
-        triangles.push_back(v2);
-        triangles.push_back(v1);
-        triangles.push_back(v2);
-        triangles.push_back(v3);
-      }
-      n++;
-    }
-  }
+	GLuint n = 0;
+	for (GLuint i = 0; i < width; i++) {
+		for (GLuint j = 0; j < height; j++) {
+			float x = 2 * (float)i / (float)(width - 1) - 1;
+			float y = 2 * (float)j / (float)(height - 1) - 1;
+			Vec3 vert(x, y, 0);
+			vposition.push_back(vert);
+			if (i > 0 && j > 0) {
+				GLuint v0 = n;
+				GLuint v1 = v0 - 1;
+				GLuint v2 = v0 - height;
+				GLuint v3 = v1 - height;
+				triangles.push_back(v0);
+				triangles.push_back(v1);
+				triangles.push_back(v2);
+				triangles.push_back(v1);
+				triangles.push_back(v2);
+				triangles.push_back(v3);
+			}
+		n++;
+		}
+	}
 
-  gpu_mesh.set_vbo<Vec3>("vposition", vposition);
-  gpu_mesh.set_triangles(triangles);
+	gpu_mesh.set_vbo<Vec3>("vposition", vposition);
+	gpu_mesh.set_triangles(triangles);
 }
 
 void DepthSurfaceRenderer::set_depth_texture(const GenericTexture &texture) {
-  if (texture.get_width() != width || texture.get_height() != height) {
-    width = texture.get_width();
-    height = texture.get_height();
-    rebuild_mesh();
-  }
+	if (texture.get_width() != width || texture.get_height() != height) {
+		width = texture.get_width();
+		height = texture.get_height();
+		rebuild_mesh();
+	}
 
-  depth_texture = &texture;
+	depth_texture = &texture;
 }
 
 void DepthSurfaceRenderer::set_sensor_matrix(const Mat4x4 &sensor_matrix) {
-  this->sensor_matrix_inv = sensor_matrix.inverse();
-  shader.bind();
-  shader.set_uniform("sensor_matrix_inv", sensor_matrix_inv);
-  shader.unbind();
+	this->sensor_matrix_inv = sensor_matrix.inverse();
+	shader.bind();
+	shader.set_uniform("sensor_matrix_inv", sensor_matrix_inv);
+	shader.unbind();
 }
 
 void DepthSurfaceRenderer::set_zfar(float zfar) {
-  this->zfar = zfar;
-  shader.bind();
-  shader.set_uniform("zfar", this->zfar);
-  shader.unbind();
+	this->zfar = zfar;
+	shader.bind();
+	shader.set_uniform("zfar", this->zfar);
+	shader.unbind();
 }
 
 void DepthSurfaceRenderer::render(const RenderContext &context) {
-  shader.bind();
+	shader.bind();
 
-  glActiveTexture(GL_TEXTURE0);
-  if (depth_texture) depth_texture->bind();
+	glActiveTexture(GL_TEXTURE0);
+	if (depth_texture) depth_texture->bind();
 
-  gpu_mesh.set_attributes(shader);
-  update_shader(shader, context);
+	gpu_mesh.set_attributes(shader);
+	update_shader(shader, context);
 
-  if (wireframe_mode == WireframeMode::WiresOnly)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	if (wireframe_mode == WireframeMode::WiresOnly)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  gpu_mesh.draw();
+	gpu_mesh.draw();
 
-  if (wireframe_mode == WireframeMode::WiresOnly)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	if (wireframe_mode == WireframeMode::WiresOnly)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-  shader.unbind();
+	shader.unbind();
 }
 
 void DepthSurfaceRenderer::rebuild() {
-  build_shader(shader, vshader(), fshader());
+	build_shader(shader, vshader(), fshader());
 
-  shader.bind();
-  gpu_mesh.set_attributes(shader);
-  shader.set_uniform("depth_texture", 0);
-  shader.set_uniform("sensor_matrix_inv", sensor_matrix_inv);
-  shader.set_uniform("zfar", zfar);
-  shader.unbind();
+	shader.bind();
+	gpu_mesh.set_attributes(shader);
+	shader.set_uniform("depth_texture", 0);
+	shader.set_uniform("sensor_matrix_inv", sensor_matrix_inv);
+	shader.set_uniform("zfar", zfar);
+	shader.unbind();
 }
 
 GPUMesh &DepthSurfaceRenderer::get_gpu_mesh() { return gpu_mesh; }
